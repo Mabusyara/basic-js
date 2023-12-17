213dff5d2eca3703354e4f09162043f5082006d6
Aleksey Sobolev <iammabus@gmail.com>
2023-12-17 03:27:04 +0300

    feat: complete mac-address and matrix-elements-sum tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  n = n.split('-');
+  if (n.length !== 6) return false;
+
+  let regexp = /[0-9a-fA-F]{2}/;
+  for (let number of n) {
+    if (!regexp.test(number)) return false;
+  }
+  return true;
 }
 module.exports = {
   isMAC48Address
